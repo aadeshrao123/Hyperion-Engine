10f6217683ae8181dc5e4feb8cc2283a0ccab70a
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-07-23 13:50:04 +0530

    Added GLM submodule

--- premake5.lua
+++ premake5.lua
@@ -18,6 +18,7 @@ IncludeDir = {}
 IncludeDir["GLFW"] = "Hyperion/vendor/GLFW/include"
 IncludeDir["Glad"] = "Hyperion/vendor/Glad/include"
 IncludeDir["ImGui"] = "Hyperion/vendor/imgui"
+IncludeDir["glm"] = "Hyperion/vendor/glm"
 
 include "Hyperion/vendor/GLFW"
 include "Hyperion/vendor/Glad"
@@ -38,7 +39,9 @@ project "Hyperion"
 	files
 	{
 		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl",		
 	}
 
 	includedirs
@@ -47,7 +50,8 @@ project "Hyperion"
 		"%{prj.name}/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}"
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}"
 	}
 
 	links 
@@ -107,7 +111,9 @@ project "Sandbox"
 	includedirs
 	{
 		"Hyperion/vendor/spdlog/include",
-		"Hyperion/src"
+		"Hyperion/src",
+		"%{IncludeDir.glm}"
+
 	}
 
 	links
